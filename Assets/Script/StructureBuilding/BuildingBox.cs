b7f2be69f15ebd69cfb5d4c493f838b83581f5d9
Yuto <naturalwater56@gmail.com>
2026-08-12 21:16:43 +0900

    feat:建築開始・終了時の効果音を追加

--- Assets/Script/StructureBuilding/BuildingBox.cs
+++ Assets/Script/StructureBuilding/BuildingBox.cs
@@ -3,8 +3,12 @@ using Utility;
 
 public class BuildingBox : MonoBehaviour
 {
+    [SerializeField] private AudioClip _buildingSound;
+    [SerializeField] private AudioClip _buildCompleteSound;
+
     private GameObject _structurePrefab;
     private GameObject _player;
+    private StructureData _structureData;
     private CountdownTimer _buildTimer;
     private Health _health;
     protected void Awake()
@@ -17,10 +21,17 @@ public class BuildingBox : MonoBehaviour
         {
             _health.OnDied += Die;
         }
+
+        const float MIN_BUILD_TIME = 1f;
+        if (_structureData.BuildTime > MIN_BUILD_TIME)
+        {
+            AudioManager.Instance.PlaySoundEffect(_buildingSound);
+        }
     }
 
     public void Init(StructureData data, GameObject player)
     {
+        _structureData = data;
         _structurePrefab = data.Prefab;
         _player = player;
         _buildTimer = new CountdownTimer(data.BuildTime);
@@ -38,6 +49,7 @@ public class BuildingBox : MonoBehaviour
         {
             Structure structure = Instantiate(_structurePrefab, transform.position, Quaternion.identity).GetComponent<Structure>();
             structure.Init(_player);
+            AudioManager.Instance.PlaySoundEffect(_buildCompleteSound);
             Destroy(gameObject);
         }
     }
